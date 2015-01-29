839ecc8b81805564f87e83c832550c86b3323163
Ryan <ryan@barnett.io>
2015-01-28 23:30:58 -0500

    complete

--- week 3/PSet3.2_isWordGuessed.py
+++ week 3/PSet3.2_isWordGuessed.py
@@ -7,4 +7,14 @@ def isWordGuessed(secretWord, lettersGuessed):
       False otherwise
     '''
     # FILL IN YOUR CODE HERE...
+    correct_letters = []
+    for letter in lettersGuessed:
+        if letter in secretWord:
+            correct_letters.append(letter)
+    for c in secretWord:
+        if c not in correct_letters: return False;
+    return True
 
+secretWord = 'apple'
+lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+print isWordGuessed(secretWord, lettersGuessed)
